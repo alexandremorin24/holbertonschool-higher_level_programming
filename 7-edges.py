5d5356836a50016ac1a9b3e313a95c3d54d6eba1
Alexandre <9761@holbertonstudents.com>
2025-01-13 13:14:26 +0100

    Fixes

--- 7-edges.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[:-3]}")
-print(f"Middle word: {word[1:-1]}")
