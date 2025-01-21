a2b51515db4e1806e8b1c6e05972ac6abe0ac97a
Alexandre <9761@holbertonstudents.com>
2025-01-21 14:00:27 +0100

    Fix task 7, 8, , 10 and 12

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,12 +3,4 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    best_key = None
-    best_value = '-inf'
-
-    for key, value in a_dictionary.item():
-        if value > best_value:
-            best_value = value
-            best_key = key
-
-    return best_key
+    return max(a_dictionary, key=a_dictionary.get)
--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    b_dictionary = {}
-    for key, value in a_dictionary.item:
-        b_dictionary[key] = value * 2
-
-    return b_dictionary
+    return {key: value * 2 for key, value in a_dictionary.items()}
