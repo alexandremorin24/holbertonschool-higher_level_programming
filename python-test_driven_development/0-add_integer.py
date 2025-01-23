78577013fa378112a8b8015547263f4279fc7023
Alexandre <9761@holbertonstudents.com>
2025-01-23 10:47:18 +0100

    Add checks for float overflow and NaN in add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -39,10 +39,26 @@ def add_integer(a, b=98):
         Traceback (most recent call last):
         ...
         TypeError: b must be an integer
+
+        >>> add_integer(2, float('-inf'))
+Traceback (most recent call last):
+    ...
+TypeError: b must be a valid number
+
+>>> add_integer(float('nan'), 2)
+Traceback (most recent call last):
+    ...
+TypeError: a must be a valid number
     """
+
     if not isinstance(a, (int, float)) or a is None:
         raise TypeError("a must be an integer")
+    if isinstance(a, float) and (a != a or a in (float('inf'), float('-inf'))):
+        raise TypeError("a must be a valid number")
+
     if not isinstance(b, (int, float)) or b is None:
         raise TypeError("b must be an integer")
+    if isinstance(b, float) and (b != b or b in (float('inf'), float('-inf'))):
+        raise TypeError("b must be a valid number")
 
     return int(a) + int(b)
