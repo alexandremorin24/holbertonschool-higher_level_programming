f9663a56e2ee683d9b0cbabc239139cbfae617a9
Alexandre <9761@holbertonstudents.com>
2025-01-23 10:01:59 +0100

    Updated tests files and added task 5

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -13,7 +13,7 @@ def say_my_name(first_name, last_name=""):
 
     Args:
         first_name (str): The first name to print.
-        last_name (str, optional): The last name to print. Defaults to an empty string.
+        last_name (str): The last name to print. Empty string by defaults.
 
     Raises:
         TypeError: If `first_name` or `last_name` is not a string.
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""
+This module provides a function `text_indentation` that prints
+a text with 2 new lines after each of
+these characters: . , ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ?, and :.
+
+    Args:
+        text (str): The text to process.
+
+    Raises:
+        TypeError: If `text` is not a string.
+
+    Example:
+        >>> text_indentation("Hello. How are you? I am fine: thanks.")
+        Hello.
+
+        How are you?
+
+        I am fine:
+
+        thanks.
+    """
+
+    if not isinstance(text, (str)):
+        raise TypeError("text must be a string")
+
+    separator = ".?:"
+
+    for sep in ".?:":
+        text = text.replace(sep, sep + "\n\n")
+
+    lines = [line.strip() for line in text.split("\n") if line.strip()]
+    formatted_text = "\n".join(lines)
+
+    print(formatted_text)
