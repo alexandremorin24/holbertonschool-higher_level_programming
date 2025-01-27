4214cfdcaa060a0de5f8c00294754f13de8a22ca
Alexandre <alexandre.morin24@gmail.com>
2025-01-27 10:47:58 +0100

    Fix task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -21,7 +21,7 @@ class Square:
             ValueError: If size is not an integer or is less than 0.
         """
         if not isinstance(size, int):
-            raise ValueError("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
