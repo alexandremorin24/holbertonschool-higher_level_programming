44a52fb18a931c63a531b8bff764f3b3e6700b9d
Alexandre <9761@holbertonstudents.com>
2025-02-03 11:26:25 +0100

    chmod +x

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """
-Returns the sorted list of available attributes.
+This module defines a class MyList that inherits from list.
 """
 
 
 class MyList(list):
+    """
+    A class that extends the built-in list class.
+    """
+
     def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+        """
         print(sorted(self))
