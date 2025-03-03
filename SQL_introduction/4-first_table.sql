65cf292a8346c2ad7d61c9918b4261f2444c9b40
Alexandre <alexandre.morin24@gmail.com>
2025-03-03 11:51:10 +0100

    Fix T2 and T4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- This script creates a table called first_table if it does not already exist
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT PRIMARY KEY,
-    name VARCHAR(255),
-    age INT
+    name VARCHAR(256)
 );
