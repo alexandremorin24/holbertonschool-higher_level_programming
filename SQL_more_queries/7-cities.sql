a5b1280a601dffd0283e84e305ab66ae9e8e0dd8
Alexandre <alexandre.morin24@gmail.com>
2025-03-04 13:14:06 +0100

    Fix T7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,10 @@
 -- This script creates the 'cities' table with an auto-increment ID, a name, and a foreign key 'state_id' referencing 'states(id)' with ON DELETE CASCADE.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE
